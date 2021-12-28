ff2d03072e0c66328f0dea2dbfd0c3bbe503acc0
Lkoenig2121 <49110001+Lkoenig2121@users.noreply.github.com>
2021-12-28 12:53:35 -0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
+      
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+      
+    mongoose.connect('mongodb://localhost/amazona', { //'mongodb://localhost:27017/amazona'
+    // useNewUrlParser: true, // a student declared this was not needed anymore
+    // useUnifiedTopology: true,
+    // useCreateIndex: true, // Not sure if this should be removed
+});
+
+// enable cors
+// app.use(cors);
+       
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});    
+
+const port = process.env.PORT || 5000;
+// const port = 5000;
+
+app.listen(5000, () => {
+    console.log('Serve at http://localhost:5000');
+});
